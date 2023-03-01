7721aab546edddc588027e949aebbf27896ea36d
xiaoyefly <xiaoyefly666@gmail.com>
2023-03-01 22:04:28 +0800

    桶排序

--- Arithmetic/Code/Practice/Day2.cs
+++ Arithmetic/Code/Practice/Day2.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Arithmetic.Code.Common;
 
 public class Day2
@@ -11,12 +13,37 @@ public class Day2
             3, 55, 32, 567, 32, 24, 6, 23, 64, 234, 234, 67, 43, 24, 124, 4
 
         };
-        HeapSort(nums);
+        // HeapSort(nums);//堆排序
+        nums=BucketSort(nums);//桶排序
         foreach (var VARIABLE in nums)
         {
             Console.Write(VARIABLE+"\n");
         }
     }
+    
+    public int[] BucketSort(int[] nums)
+    {
+        int maxNum = nums.Max();
+        int bucketSize = maxNum / nums.Length + 1;
+        List<int>[] buckets = new List<int>[bucketSize];
+        for (int i = 0; i < bucketSize; i++)
+        {
+            buckets[i] = new List<int>();
+        }
+        foreach (int num in nums)
+        {
+            int index = num / bucketSize;
+            buckets[index].Add(num);
+        }
+        List<int> result = new List<int>();
+        foreach (List<int> bucket in buckets)
+        {
+            result.AddRange(bucket.OrderBy(x => x));
+        }
+        return result.ToArray();
+    }
+
+    
     void Heapify(int[] nums, int n, int i)
     {
         int largest = i;
